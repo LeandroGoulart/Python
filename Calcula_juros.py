29ec99396b023c4a22ab37e662301089c975fa71
LGoulart <leandrovgoulart@gmail.com>
2022-10-16 18:30:36 -0300

    correção de variáveis

--- Calcula_juros.py
+++ Calcula_juros.py
@@ -2,15 +2,16 @@
 """
 Fórmula de juros composto
 M=C(1+i)^t
+Legenda
 M = montante
 C = capital
 i = taxa de juros
 t = tempo
 """
-C = int(input("\nInforme capital para investimento: "))
+C = float(input("\nInforme capital para investimento: "))
 i = 15/1000
-t = int(input("\nTempo de aplicação em meses: "))
+t = float(input("\nTempo de aplicação em meses: "))
 M=C*(1 + i)**t
-print ('Juros de retorno= ',M-C)
+print ('\nJuros de retorno= ', M-C)
 res = "{:.2f}".format(M)
-print ('Seu rendimento total será: ',res)
+print ('\nSeu rendimento total será: {} \n' .format(res)) 
